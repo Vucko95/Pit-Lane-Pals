6aa312b9c07eb41ad0bda3605c3225ea4686642d
Ronald Suplina <tyger995@gmail.com>
2023-09-10 17:52:37 +0100

    Navbar Edits

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,27 @@
 "use clinet";
 import { Menu } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image"
+import LogoSVG from "./LogoSVG";
 import { ModeToggle } from "@/app/components/mode-toggle"
 import { Button } from "./ui/button";
 import {MobileSidebar} from "@/app/components/mobile-sidebar"
 export const Navbar = () => {
     return (
-        <div className="fixed w-full  z-50 flex justify-between items-center bg-secondary py-2 px-4 border-b-2 h-16">
-            <Link href="/">
-            <h1 className=" hidden md:block text-2xl ">Pit Lane Pals</h1>
-            </Link>
-            <div className="flex items-center">
-                <MobileSidebar/>
-                {/* <Menu className="block md:hidden" /> */}
+        <div className="fixed w-full z-50">
+            <div className="flex justify-center items-center py-2 px-4 border-b-2 h-16">
+                <Link href="/">
+                    <div className="flex items-center">
+                        <LogoSVG />
+                    </div>
+                </Link>
+                    <div className="mr-10 ">
+                        <MobileSidebar />
+                    </div>
+                <div className="absolute top-3 right-3 h-full items-center hidden md:block">
+                    <ModeToggle />
+                </div>
             </div>
-            <ModeToggle/>
         </div>
     )
 }
