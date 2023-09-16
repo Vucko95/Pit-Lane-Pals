3a5732487768cb580f3d7c2ef7ed1ebb0655337b
Ronald Suplina <tyger995@gmail.com>
2023-09-16 11:20:49 +0100

    Edit Categories

--- app/components/categories.tsx
+++ app/components/categories.tsx
@@ -16,8 +16,8 @@ export const Categories = ( { data }: CategoriesProps) => {
     const categoryId = searchParams.get("categoryId");
   
     const onClick = (id: string | undefined) => {
+
       const query = { categoryId: id };
-  
       const url = qs.stringifyUrl({
         url: window.location.href,
         query
@@ -33,11 +33,12 @@ export const Categories = ( { data }: CategoriesProps) => {
     return (
       <div className="w-full  space-x-2 flex flex-wrap justify-center items-center p-1 ">
         {
-          <button onClick={() => router.push("/companion/new")} className={cn( ` flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 m-1 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition `, !categoryId ? "bg-primary/25" : "bg-primary/10" )} >
-             Create Companion </button>
+          <button onClick={() => router.push("/companion/new")}
+           className={cn( ` flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 m-1 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition `, !categoryId ? "bg-primary/10" : "bg-primary/5 border-orange-500 border-2" )} >
+             Create your F1 Persona  </button>
         }
         {data.map((item) => (
-          <button onClick={() => onClick(item.id)} className={cn( ` flex items-center text-center text-xs md:text-sm px-2 md:px-4 m-1 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition `, item.id === categoryId ? "bg-primary/25" : "bg-primary/10" )} key={item.id} > {item.name} </button>
+          <button onClick={() => onClick(item.id)} className={cn( ` flex items-center text-center text-xs md:text-sm px-2 md:px-4 m-1 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition `, item.id === categoryId ? "bg-primary/20" : "bg-primary/10" )} key={item.id} > {item.name} </button>
         ))}
       </div>
     );
--- app/scripts/seed.ts
+++ app/scripts/seed.ts
@@ -1,24 +1,24 @@
-const { PrismaClient } = require('@prisma/client');
+// const { PrismaClient } = require('@prisma/client');
 
-const db = new PrismaClient();
+// const db = new PrismaClient();
 
-async function main() {
-  try {
-    await db.category.createMany({
-      data: [
-        { name: 'Drivers' },
-        { name: 'Team Principals' },
-        { name: 'All-Stars' },
-        { name: 'Famous Persons' },
-        { name: 'Fans' },
-        { name: 'Bots' },
-      ],
-    });
-  } catch (error) {
-    console.error('Error seeding default categories:', error);
-  } finally {
-    await db.$disconnect();
-  }
-}
+// async function main() {
+//   try {
+//     await db.category.createMany({
+//       data: [
+//         { name: 'Drivers' },
+//         { name: 'Team Principals' },
+//         { name: 'All-Stars' },
+//         { name: 'Famous Persons' },
+//         { name: 'Fans' },
+//         { name: 'Bots' },
+//       ],
+//     });
+//   } catch (error) {
+//     console.error('Error seeding default categories:', error);
+//   } finally {
+//     await db.$disconnect();
+//   }
+// }
 
-main();
+// main();
